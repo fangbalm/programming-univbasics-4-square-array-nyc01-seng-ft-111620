55b39a74c0ed4b22008795e8c0e60283ddb31f0f
Lucius VanSlyke <lu.vanslyke@gmail.com>
2020-10-21 22:33:33 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,12 +3,26 @@ def square_array(array)
   count = 0
   new_array = []
   while count < array.length do
-    new_array.push(Math.sqrt(array[count]))
-    count = count + 1
+    new_array.push(array[count] ** array[count])
+    count = count + 1 
   end
   return new_array
 end
 
+
+
+
+# def square_array(array)
+#   # your code here
+#   count = 0
+#   new_array = []
+#   while count < array.length do
+#     new_array.push(Math.sqrt(array[count]))
+#     count = count + 1
+#   end
+#   return new_array
+# end
+# 
 
 
 # def square_array(array)
